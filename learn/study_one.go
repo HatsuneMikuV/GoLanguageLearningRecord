78e7810d88eda8f2f1677a4509d53b0734074f50
Icarus <15188588180@163.com>
2018-01-29 18:51:33 +0800

    The first day of study

---
+++ learn/study_one.go
@@ -0,0 +1,10 @@
+
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("你好")
+	fmt.Printf("hello world")
+}
+
---
+++ learn/study_two.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main()  {
+
+	min, max := 0, 100
+	fmt.Printf("请想一个%d~%d的整数。\n", min, max)
+
+	for min < max  {
+		i := (min + max) / 2
+		fmt.Printf("该数小于或者等于%d吗？（y/n）", i)
+		var  s string
+		fmt.Scanf("%s", &s)
+		if s != "" && s[0] == 'y' {
+			max = i
+		}else {
+			min = i + 1
+		}
+	}
+	fmt.Printf("该数是%d\n", max)
+
+	//go 语言  提供的二分查找功能
+	fmt.Println("Pick a number from 0 to 100")
+	fmt.Printf("Your number is %d\n", sort.Search(100, func(i int) bool {
+		fmt.Printf("Is your number <= %d?", i)
+		var s string
+		fmt.Scanf("%s\n", &s)
+		return s != "" && s[0] == 'y'
+	}))
+
+}
