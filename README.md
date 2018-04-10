c619c809698e0cef74c8b3f6b6951f30c32bbd9e
Icarus <anglemiku_v@163.com>
2018-04-10 17:58:09 +0800

    update

--- README.md
+++ README.md
@@ -7,4 +7,11 @@ go  语言学习记录
 #### 学习者有一定的C语言基础
 
 
-##### 欢迎各方人士指导，感激不尽！！！
+### Progress 进度
+
+- Program  [《Go语言·云动力》](https://github.com/HatsuneMikuV/GoLanguageLearningRecord/blob/master/learn/Go%E8%AF%AD%E8%A8%80%C2%B7%E4%BA%91%E5%8A%A8%E5%8A%9B.pdf)
+- Program  [《Go 语言圣经》](https://github.com/HatsuneMikuV/GoLanguageLearningRecord/blob/master/go%E8%AF%AD%E8%A8%80%E5%9C%A3%E7%BB%8F/Go%20%E8%AF%AD%E8%A8%80%E5%9C%A3%E7%BB%8F%20%E4%B8%AD%E6%96%87%E7%89%88.pdf)
+
+
+
+##### 欢迎指正、纠错，QQ479069761
--- go语言圣经/study_03.go
+++ go语言圣经/study_03.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/png"
 	"log"
 	"math"
 	"math/cmplx"
 	"net/http"
 	"os"
+	"time"
 )
 
 /* 基础数据类型 */
@@ -107,6 +111,7 @@ func test_two03()  {
 	handle := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "image/svg+xml")
 		if err := r.ParseForm(); err != nil{
+			fmt.Println(err)
 			return
 		}
 		fmt.Fprintf(w, getSvg())
@@ -127,17 +132,20 @@ var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 
 func corner(i, j int) (float64, float64) {
 	// Find point (x,y) at corner of cell (i,j).
-	x := xyrange * (float64(i)/cells - 0.5)
-	y := xyrange * (float64(j)/cells - 0.5)
+	x := xyrange * (float64(i) / cells - 0.5)
+	y := xyrange * (float64(j) / cells - 0.5)
 	// Compute surface height z.
 	z := f(x, y)
 	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
-	sx := width/2 + (x-y)*cos30*xyscale
-	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	sx := width / 2 + (x - y) * cos30 * xyscale
+	sy := height / 2 + (x + y) * sin30 * xyscale - z * zscale
 	return sx, sy
 }
 func f(x, y float64) float64 {
 	r := math.Hypot(x, y) // distance from (0,0)
+	if math.IsNaN(r) || math.IsInf(r, 0) {
+		return 0
+	}
 	return math.Sin(r) / r
 }
 func getSvg()(svg string)  {
@@ -179,15 +187,209 @@ func test_thr03()  {
 	fmt.Println(imag(xx * yy))
 
 	fmt.Println(cmplx.Sqrt(-1))
+
+
+	//绘制Mandelbrot图像
+	getMandelbrot("mandelbrot1.png", 1)//黑白
+	getMandelbrot("mandelbrot2.png", 2)//彩色
+	getMandelbrot("mandelbrot3.png", 3)//网格
+	getMandelbrot("mandelbrot4.png", 4)//彩色
+	getMandelbrot("mandelbrot5.png", 5)//彩色
+	getMandelbrot("mandelbrot6.png", 6)//彩色
+	getMandelbrot("mandelbrot7.png", 7)//彩色
+	getMandelbrot("mandelbrot8.png", 8)//彩色
+	getMandelbrot("mandelbrot9.png", 9)//彩色
 }
+func getMandelbrot(fileName string, intType int)  {
+	//绘制Mandelbrot图像
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height = 1024, 1024
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		yyy := float64(py) / height * (ymax - ymin) + ymin
+		for px := 0; px < width; px++ {
+			xxx := float64(px) / width * (xmax - xmin) + xmin
+			z := complex(xxx, yyy)
+			if intType == 1 {
+				img.Set(px, py, mandelbrot(z))
+			}else if intType == 2 {
+				img.Set(px, py, sqrt(z))
+			}else if intType == 3 {
+				img.Set(px, py, newton(z))
+			}else if intType == 4 {
+				img.Set(px, py, acos(z))
+			}else if intType == 5 {
+				img.Set(px, py, aCosh(z))
+			}else if intType == 6 {
+				img.Set(px, py, asin(z))
+			}else if intType == 7 {
+				img.Set(px, py, aSinh(z))
+			}else if intType == 8 {
+				img.Set(px, py, atan(z))
+			}else if intType == 9 {
+				img.Set(px, py, aTanh(z))
+			}
+		}
+	}
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	defer file.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	png.Encode(file, img)
+}
+func mandelbrot(z complex128) color.Color  {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v * v + z
+		if cmplx.Abs(v) > 2 {
+			return color.Gray{255 - contrast * n}
+		}
+	}
+	return color.Black
+}
+func acos(z complex128) color.Color {
+	v := cmplx.Acos(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{192, blue, red}
+}
+func aCosh(z complex128) color.Color {
+	v := cmplx.Acosh(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{192, blue, red}
+}
+func asin(z complex128) color.Color {
+	v := cmplx.Asin(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{192, blue, red}
+}
+func aSinh(z complex128) color.Color {
+	v := cmplx.Asinh(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{192, blue, red}
+}
+func atan(z complex128) color.Color {
+	v := cmplx.Atan(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{192, blue, red}
+}
+func aTanh(z complex128) color.Color {
+	v := cmplx.Atanh(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{192, blue, red}
+}
+
+func sqrt(z complex128) color.Color {
+	v := cmplx.Sqrt(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{128, blue, red}
+}
+
+func newton(z complex128) color.Color {
+	const iterations = 37
+	const contrast = 7
+	for i := uint8(0); i < iterations; i++ {
+		z -= (z - 1/(z*z*z)) / 4
+		if cmplx.Abs(z*z*z*z-1) < 1e-6 {
+			return color.Gray{255 - contrast*i}
+		}
+	}
+	return color.Black
+}
+
+//布尔型
+func test_fou03()  {
+	var s string
+	if s != "" && s[0] == 'x' {
+		fmt.Println(s)
+	}else {
+		fmt.Println("s is nil")
+	}
+
+	var c int
+	if 'a' <= c && c <= 'z' ||
+		'A' <= c && c <= 'Z' ||
+		'0' <= c && c <= '9'{
+		fmt.Println(c)
+	}else {
+		fmt.Println("c is not in [a, z][A, Z][0, 9]")
+	}
+}
+
+//字符串
+func test_fiv03()  {
+	s := "hello,world"
+	fmt.Println(len(s))
+	fmt.Println(s[0],s[7])
+
+	fmt.Println(s[0:5])
+	fmt.Println(s[:5])
+	fmt.Println(s[7:])
+	fmt.Println(s[7:len(s)])
+	fmt.Println(s[:])
+	fmt.Println("goodbye" + s[5:])
+
+	ss := "left foot"
+	t := ss
+	ss += ",right foot"
+	fmt.Println(t)
+	fmt.Println(ss)
+}
+
+//常量
+func test_six03()  {
+	const  pi  = 3.14159
+	const (
+		e = 2.71828182845904523536028747135266249775724709369995957496696763
+		ppi = 3.14159265358979323846264338327950288419716939937510582097494459
+	)
+
+	const noDelay time.Duration = 0
+	const timeout = 5 * time.Minute
+	fmt.Printf("%T %[1]v\n", noDelay)
+	fmt.Printf("%T %[1]v\n", timeout)
+	fmt.Printf("%T %[1]v\n", time.Minute)
+
+	const (
+		a = 1
+		b
+		c = 2
+		d
+	)
+	fmt.Println(a, b, c, d)
+}
+
 func main()  {
 
 	//整型
-	test_one03()
+	//test_one03()
 
 	//浮点数
 	//test_two03()
 
 	//复数
-	test_thr03()
+	//test_thr03()
+
+	//布尔型
+	test_fou03()
+
+	//字符串
+	test_fiv03()
+
+	//常量
+	test_six03()
 }
