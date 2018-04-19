9fdefac7b8f03156bf606c119cb93d5b1d0edd98
Icarus <anglemiku_v@163.com>
2018-04-19 17:30:17 +0800

    update

--- go语言圣经/study_05.go
+++ go语言圣经/study_05.go
@@ -5,7 +5,7 @@ import (
 	"golang.org/x/net/html"
 	"math"
 	"net/http"
-	"os"
+	"strings"
 )
 
 /* 函数 */
@@ -52,7 +52,7 @@ func test_recursive() {
 
 	resp, err := http.Get("https://golang.org")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		fmt.Printf("fetch: %v\n", err)
 		return
 	}
 
@@ -60,7 +60,7 @@ func test_recursive() {
 	resp.Body.Close()
 
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		fmt.Printf("findlinks: %v\n", err)
 		return
 	}
 
@@ -183,11 +183,114 @@ func visit_recursive(links []string, n *html.Node) []string {
 	}
 	return links
 }
+
+//多返回值
+//1.一个函数可以返回多个值
+//2.调用多返回值函数时，必须显式的将值分配给变量，或者分配给_(blank identifier)
+func test_more()  {
+	links, err := findLinks("https://golang.org")
+	if err != nil {
+		fmt.Printf("findlinks: %v\n", err)
+	}
+	for _, link := range links {
+		fmt.Println(link)
+	}
+	fmt.Println("--------------------------分割线--------------------------")
+
+	//练习 5.5： 实现countWordsAndImages。（参考练习4.9如何分词）
+	words, images, err := CountWordsAndImages("https://golang.org")
+	fmt.Println(words, images, err)
+}
+//练习 5.6： 修改gopl.io/ch3/surface (§3.2) 中的corner函数，将返回值命名，并使用bare return。
+func corner_more(i, j int) (sx, sy float64) {
+	// Find point (x,y) at corner of cell (i,j).
+	x := xyrange * (float64(i) / cells - 0.5)
+	y := xyrange * (float64(j) / cells - 0.5)
+	// Compute surface height z.
+	z := f(x, y)
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
+	sx = width / 2 + (x - y) * cos30 * xyscale
+	sy = height / 2 + (x + y) * sin30 * xyscale - z * zscale
+
+	return
+}
+// findLinks performs an HTTP GET request for url, parses the
+// response as HTML, and extracts and returns the links.
+func findLinks(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+	return visit(nil, doc), nil
+}
+//3.如果函数的返回值都是现实的变量名，则该函数的return可以省略操作数, 这称之为bare return
+// CountWordsAndImages does an HTTP GET request for the HTML
+// document url and returns the number of words and images in it.
+func CountWordsAndImages(url string) (words, images int, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		err = fmt.Errorf("parsing HTML: %s", err)
+		return
+	}
+	words, images = countWordsAndImages(doc)
+	return
+}
+//练习 5.5： 实现countWordsAndImages。（参考练习4.9如何分词）
+func countWordsAndImages(n *html.Node) (words, images int) {
+
+	txts, images := visit_words(nil, n, 0)
+	for _, word := range txts {
+		wordss := strings.Fields(word)
+		if len(wordss) > 0 {
+			words += len(wordss)
+		}
+	}
+	return
+}
+
+func visit_words(links []string, n *html.Node, img int) ([]string, int)  {
+	if n.Type == html.TextNode {
+		links = append(links, n.Data)
+	}
+	if n.Type == html.ElementNode && n.Data == "img" {
+		img++
+	}
+
+	if n.FirstChild != nil {
+		if n.FirstChild.Data != "script" && n.FirstChild.Data != "style" {
+			links, img = visit_words(links, n.FirstChild, img)
+		}
+	}
+	if n.NextSibling != nil {
+		if n.NextSibling.Data != "script" && n.NextSibling.Data != "style" {
+			links, img = visit_words(links, n.NextSibling, img)
+		}
+	}
+	return links, img
+}
+
 func main() {
 
 	//函数声明
-	test_func()
+	//test_func()
 
 	//递归
-	test_recursive()
+	//test_recursive()
+
+	//多返回值
+	test_more()
 }
