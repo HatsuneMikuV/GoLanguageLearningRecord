148ca04140658d11ac8814aafb5cc5576e09ceeb
Icarus <anglemiku_v@163.com>
2018-04-20 18:48:48 +0800

    update

--- go语言圣经/study_05.go
+++ go语言圣经/study_05.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"golang.org/x/net/html"
+	"io/ioutil"
 	"log"
 	"math"
 	"net/http"
+	"net/url"
+	"os"
+	"sort"
 	"strings"
 	"time"
 )
@@ -460,6 +464,267 @@ func expand(s string, f func(string) string) string {
 func foo(s string) string  {
 	return s + "+header+"
 }
+
+//匿名函数
+//1.匿名函数字面量的语法和函数声明相似，区别在于func关键字后没有函数名
+//2.匿名函数不仅仅是一串代码，还记录状态，因此属于引用类型，且函数值不可比较
+//3.Go使用闭包（closures）技术实现函数值，Go程序员也把函数值叫做闭包
+func test_anonymous()  {
+	f := squares()
+	fmt.Println(f()) // "1"
+	fmt.Println(f()) // "4"
+	fmt.Println(f()) // "9"
+	fmt.Println(f()) // "16"
+
+	// prereqs记录了每个课程的前置课程
+	var prereqs = map[string][]string{
+		"algorithms": {"data structures"},
+		"calculus": {"linear algebra"},
+		"linear algebra": {"calculus"},
+		"compilers": {
+			"data structures",
+			"formal languages",
+			"computer organization",
+		},
+		"data structures":       {"discrete math"},
+		"databases":             {"data structures"},
+		"discrete math":         {"intro to programming"},
+		"formal languages":      {"discrete math"},
+		"networks":              {"operating systems"},
+		"operating systems":     {"data structures", "computer organization"},
+		"programming languages": {"data structures", "computer organization"},
+	}
+
+	for i, course := range topoSort(prereqs){
+		fmt.Printf("%d:\t%s\n", i + 1, course)
+	}
+
+	breadthFirst(crawl, []string{"https://golang.org"})
+
+	//练习5.10： 重写topoSort函数，用map代替切片并移除对key的排序代码。
+	//验证结果的正确性（结果不唯一）。
+	for k, v := range topoSort_Map(prereqs) {
+		fmt.Printf("%d:\t%s\n", k, v)
+	}
+
+	//练习5.11： 现在线性代数的老师把微积分设为了前置课程。
+	//完善topSort，使其能检测有向图中的环。
+	//能力有限，暂时不知道怎么搞，尝试却没得到结果
+
+	//练习5.12： 
+	//gopl.io/ch5/outline2（5.5节）的startElement和endElement共用了全局变量depth，
+	//将它们修改为匿名函数，使其共享outline中的局部变量。
+	outline2()
+}
+//练习5.13： 修改crawl，使其能保存发现的页面，
+//必要时，可以创建目录来保存这些页面。
+//只保存来自原始域名下的页面。
+//假设初始页面在golang.org下，就不要保存vimeo.com下的页面。
+func save_url(u string)  {
+
+	resp, err := http.Get(u)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("此资源不存在\n")
+		resp.Body.Close()
+		return
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	if err != nil {
+		log.Println("read data failed:", u, err)
+		return
+	}
+
+	urll, _ := url.Parse(u)
+	tmp := strings.TrimLeft(urll.Path, "/")
+	filename := strings.ToLower(strings.Replace(tmp, "/", "-", -1))
+	filename = "crawl/"+ filename
+	if !strings.Contains(filename, "html") {
+		filename += ".html"//确保保存下来的都是科打开的html文件
+	}
+	newFile, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755)
+	if err != nil {
+		fmt.Println("create file failed:", filename, err)
+		return
+	}
+	newFile.Write(data)
+	newFile.Close()
+	data = nil
+	return
+}
+func outline2()  {
+	resp, err := http.Get("https://golang.org")
+	if err != nil {
+		return
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		err = fmt.Errorf("parsing HTML: %s", err)
+		return
+	}
+
+	var depth int
+	var startElementl func(n *html.Node)
+	startElementl = func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			s := ""
+			for _, v := range n.Attr{
+				s += "  " + v.Key + "=\"" + v.Val + "\"  "
+			}
+			fmt.Printf("%*s<%s%s", depth * 2, "", n.Data, s)
+			depth++
+		}
+		if n.Type == html.ElementNode  {
+			if n.FirstChild == nil && n.Data != "script" {
+				fmt.Printf("/")
+			}
+			fmt.Printf(">\n")
+		}
+		if n.Type == html.TextNode {
+			fmt.Printf("%*s %s\n", depth * 2, "", n.Data)
+		}
+	}
+	var endElementl func(n *html.Node)
+	endElementl = func(n *html.Node){
+		depth--
+		if n.Type == html.ElementNode {
+			if n.FirstChild == nil && n.Data != "script" {
+				fmt.Printf("\n")
+			}
+			fmt.Printf("%*s</%s>\n", depth * 2, "", n.Data)
+		}
+	}
+
+	forEachNode(doc, startElementl, endElementl)
+}
+func topoSort_Map(m map[string][]string) map[int]string  {
+	seen := make(map[string]bool)
+
+	order := make(map[int]string)
+	index := 1
+	var visitAll func(items []string)
+	visitAll = func(items []string) {
+		for _, key := range items{
+			if !seen[key] {
+				seen[key] = true
+				visitAll(m[key])
+				order[index] = key
+				index++
+			}
+		}
+	}
+	keys := []string{}
+	for key := range m{
+		keys = append(keys, key)
+	}
+	visitAll(keys)
+
+	return order
+}
+func crawl(url string) []string {
+
+	//练习5.13
+	go save_url(url)
+	fmt.Println(url)
+	list, err := Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
+// breadthFirst calls f for each item in the worklist.
+// Any items returned by f are added to the worklist.
+// f is called at most once for each item.
+// 广度优先遍历
+func breadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
+// Extract makes an HTTP GET request to the specified URL, parses
+// the response as HTML, and returns the links in the HTML document.
+func Extract(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+	var links []string
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key != "href" {
+					continue
+				}
+				link, err := resp.Request.URL.Parse(a.Val)
+				if err != nil {
+					continue // ignore bad URLs
+				}
+				links = append(links, link.String())
+			}
+		}
+	}
+	forEachNode(doc, visitNode, nil)
+	return links, nil
+}
+// 深度优先遍历
+func topoSort(m map[string][]string) (ss []string)  {
+	seen := make(map[string]bool)
+
+	//4.匿名函数需要递归时，必须提前声明，否则无法递归绑定
+	var visitAll func(items []string)
+	visitAll = func(items []string) {
+		for _, item := range items{
+			if !seen[item] {
+				seen[item] = true
+				visitAll(m[item])
+				ss = append(ss, item)
+			}
+		}
+	}
+
+	var keys []string
+	for key := range m{
+		keys = append(keys, key)
+	}
+
+	sort.Strings(keys)
+	visitAll(keys)
+	return
+}
+// squares返回一个匿名函数。
+// 该匿名函数每次被调用时都会返回下一个数的平方。
+func squares() func() int {
+	var x int
+	return func() int {
+		x++
+		return x * x
+	}
+}
 func main() {
 
 	//函数声明
@@ -478,5 +743,8 @@ func main() {
 	//}
 
 	//函数值
-	test_funcV()
+	//test_funcV()
+
+	//匿名函数
+	test_anonymous()
 }
