ee9a68ef59c73bbdd4ff34ffdf351d5618af1bbb
Icarus <anglemiku_v@163.com>
2018-04-23 20:40:18 +0800

    update

--- go语言圣经/geometry/geometry.go
+++ go语言圣经/geometry/geometry.go
@@ -1,6 +1,12 @@
 package geometry
 
-import "math"
+import (
+	"bytes"
+	"fmt"
+	"image/color"
+	"math"
+	"sync"
+)
 
 type Point struct{ X, Y float64 }
 
@@ -39,4 +45,142 @@ func PathDistance(path Path) float64 {
 		}
 	}
 	return sum
+}
+
+//当你根据一个变量来决定调用同一个类型的哪个函数时，方法表达式就显得很有用了。
+//你可以根据选择来调用接收器各不相同的方法
+func (p Point) Add(q Point) Point { return Point{p.X + q.X, p.Y + q.Y} }
+func (p Point) Sub(q Point) Point { return Point{p.X - q.X, p.Y - q.Y} }
+
+func (path Path) TranslateBy(offset Point, add bool) {
+	var op func(p, q Point) Point
+	if add {
+		op = Point.Add
+	} else {
+		op = Point.Sub
+	}
+	for i := range path {
+		// Call either path[i].Add(offset) or path[i].Sub(offset).
+		path[i] = op(path[i], offset)
+	}
+}
+
+//方法的名字是(*Point).ScaleBy。
+//这里的括号是必须的，没有括号的话这个表达式可能会被理解为*(Point.ScaleBy)
+func (p *Point) ScaleBy(factor float64) {
+	p.X *= factor
+	p.Y *= factor
+}
+
+type ColoredPoint struct {
+	Point
+	Color color.RGBA
+}
+
+type ColoredPointP struct {
+	*Point
+	Color color.RGBA
+}
+
+var cache = struct {
+	sync.Mutex
+	mapping map[string]string
+}{
+	mapping: make(map[string]string),
+}
+//因为sync.Mutex字段也被嵌入到了这个struct里，其Lock和Unlock方法也就都被引入到了这个匿名结构中了，
+//这让我们能够以一个简单明了的语法来对其进行加锁解锁操作
+func Lookup(key string) string {
+	cache.Lock()
+	v := cache.mapping[key]
+	cache.Unlock()
+	return v
+}
+
+
+// An IntSet is a set of small non-negative integers.
+// Its zero value represents the empty set.
+type IntSet struct {
+	words []uint64
+}
+
+// Has reports whether the set contains the non-negative value x.
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+// Add adds the non-negative value x to the set.
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+// UnionWith sets s to the union of s and t.
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+// String returns the set as a string of the form "{1 2 3}".
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+//练习6.1: 为bit数组实现下面这些方法
+// return the number of elements
+func (s *IntSet) Len() int {
+	len := 0
+	for _, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				len++
+			}
+		}
+	}
+	return len
+}
+// remove x from the set
+func (s *IntSet) Remove(x int) {
+	ok := s.Has(x)
+	if ok {
+		
+	}
+}
+// remove all elements from the set
+func (s *IntSet) Clear()  {
+	if s.Len() > 0 {
+		
+	}
+}
+// return a copy of the set
+func (s *IntSet) Copy() *IntSet {
+
+	return nil
 }
--- go语言圣经/study_06.go
+++ go语言圣经/study_06.go
@@ -3,6 +3,8 @@ package main
 import (
 	"20180408/geometry"
 	"fmt"
+	"image/color"
+	"net/url"
 )
 
 /* 方法 */
@@ -34,9 +36,170 @@ func test_fun()  {
 	fmt.Println(geometry.PathDistance(perimm)) // "12", standalone function
 	fmt.Println(perim.Distance())
 }
+//二，基于指针对象的方法
+//1.当变量比较大时，为了节省内存是可以选择指针变量，这样就不会复制变量本身
+func test_point()  {
+
+	r := &geometry.Point{1, 2}
+	r.ScaleBy(2)
+	fmt.Println(*r)
+
+	p := geometry.Point{1, 2}
+	pptr := &p
+	pptr.ScaleBy(2)
+	fmt.Println(p)
+
+	pp := geometry.Point{1, 2}
+	(&pp).ScaleBy(2)
+	fmt.Println(pp)
+
+	//2.编译器会隐式地帮我们用&p去调用ScaleBy这个方法
+	//3.不管你的method的receiver是指针类型还是非指针类型，都是可以通过指针/非指针类型进行调用的，编译器会帮你做类型转换
+	//4.声明方法是否为指针类型取决于变量本身的大小，因为非指针类型变量会拷贝一份，而指针类型要注意内存地址，永远指向一份
+	pp.ScaleBy(3)
+	fmt.Println(pp)
+
+	//5.Nil也是一个合法的接收器类型
+	//6.传入的是存储了内存地址的变量，你改变这个变量是影响不了原始的变量的
+	m := url.Values{"lang": {"en"}}
+	m.Add("item", "1")
+	m.Add("item", "2")
+
+	fmt.Println("------------------")
+	fmt.Println(m.Get("lang"))
+	fmt.Println(m.Get("q"))
+	fmt.Println(m.Get("item"))
+	fmt.Println(m["item"])
+
+	fmt.Println("------------------")
+	m = nil
+	fmt.Println(m.Get("item"))
+	//m.Add("item", "3")//panic: assignment to entry in nil map
+}
+
+
+//三，通过嵌入结构体来扩展类型
+//1.内嵌可以使定义时的简写形式，并使其包含嵌入类型所具有的一切字段，类似OC的继承
+func test_extion()  {
+
+	var cp geometry.ColoredPoint
+	cp.X = 1
+	fmt.Println(cp.Point.X) // "1"
+	cp.Point.Y = 2
+	fmt.Println(cp.Y) // "2"
+	fmt.Println("------------------")
+
+	//2.内嵌可以使多字段类型定义时，划分成多个小类型，然后再定义小类型方法，方便于读写
+	//	可以把多字段类型认为是小类型的继承或者子类，就像OC那样，但是3...
+	red := color.RGBA{255, 0, 0, 255}
+	blue := color.RGBA{0, 0, 255, 255}
+	var p = geometry.ColoredPoint{geometry.Point{1, 1}, red}
+	var q = geometry.ColoredPoint{geometry.Point{5, 4}, blue}
+	fmt.Println(p.Distance(q.Point)) // "5"
+	p.ScaleBy(2)
+	q.ScaleBy(2)
+	fmt.Println(p.Distance(q.Point))
+	fmt.Println("------------------")
+	//3.但是并不能利用隐式的方式调用小类型的方法，因为类型不同，必须显式的调用
+	//p.Distance(q) // compile error: cannot use q (ColoredPoint) as Point
+
+	p1 := geometry.ColoredPointP{&geometry.Point{1, 1}, red}
+	q1 := geometry.ColoredPointP{&geometry.Point{5, 4}, blue}
+	fmt.Println(p1.Distance(*q1.Point)) // "5"
+	q1.Point = p1.Point                 // p and q now share the same Point
+	p1.ScaleBy(2)
+	fmt.Println(*p1.Point, *q1.Point)
+}
+
+//四，方法值和方法表达式
+func test_func_value()  {
+
+	p := geometry.Point{1, 2}
+	q := geometry.Point{4, 6}
+
+	//p.Distance作为选择器，distanceFromP一个方法"值"
+	//一个将方法(Point.Distance)绑定到特定接收器变量的函数
+	distanceFromP := p.Distance        // method value
+	fmt.Println(distanceFromP(q))      // "5"
+	var origin geometry.Point                   // {0, 0}
+	fmt.Println(distanceFromP(origin)) // "2.23606797749979", sqrt(5)
+
+	scaleP := p.ScaleBy // method value
+	scaleP(2)           // p becomes (2, 4)
+	scaleP(3)           //      then (6, 12)
+	scaleP(10)          //      then (60, 120)
+
+	{//{}目的和上面的变量区分，防止冲突
+
+		// 译注：这个Distance实际上是指定了Point对象为接收器的一个方法func (p Point) Distance()，
+		// 但通过Point.Distance得到的函数需要比实际的Distance方法多一个参数，
+		// 即其需要用第一个额外参数指定接收器，后面排列Distance方法的参数。
+		// 看起来本书中函数和方法的区别是指有没有接收器，而不像其他语言那样是指有没有返回值。
+		p := geometry.Point{1, 2}
+		q := geometry.Point{4, 6}
+
+		distance := geometry.Point.Distance   // method expression
+		fmt.Println(distance(p, q))  // "5"
+		fmt.Printf("%T\n", distance) // "func(Point, Point) float64"
+
+		scale := (*geometry.Point).ScaleBy
+		scale(&p, 2)
+		fmt.Println(p)            // "{2 4}"
+		fmt.Printf("%T\n", scale) // "func(*Point, float64)"
+	}
+}
+
+//五，示例: Bit数组
+//1.在数据流分析领域，集合元素通常是一个非负整数，集合会包含很多元素，
+//并且集合会经常进行并集、交集操作，这种情况下，bit数组会比map表现更加理想
+//2.一个bit数组通常会用一个无符号数或者称之为“字”的slice来表示，
+//每一个元素的每一位都表示集合里的一个值
+func test_bit()  {
+
+	var x, y geometry.IntSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	fmt.Println(x.String()) // "{1 9 144}"
+
+	y.Add(9)
+	y.Add(42)
+	fmt.Println(y.String()) // "{9 42}"
+
+	x.UnionWith(&y)
+	fmt.Println(x.String()) // "{1 9 42 144}"
+	fmt.Println(x.Has(8), x.Has(123))
+
+	fmt.Println(&x)         // "{1 9 42 144}"
+	fmt.Println(x.String()) // "{1 9 42 144}"
+	fmt.Println(x)          // "{[4398046511618 0 65536]}"
+
+	//练习6.1: 为bit数组实现下面这些方法
+	fmt.Println(x.Len())
+	fmt.Println("======")
+	x.Remove(1)
+	fmt.Println(&x)
+	fmt.Println("======")
+
+	x.Clear()
+	fmt.Println(&x)
+}
 
 func main() {
 
-	//方法声明
-	test_fun()
+	//一，方法声明
+	//test_fun()
+
+	//二，基于指针对象的方法
+	//test_point()
+
+	//三，通过嵌入结构体来扩展类型
+	//test_extion()
+
+	//四，方法值和方法表达式
+	//test_func_value()
+
+	//五，示例: Bit数组
+	test_bit()
 }
+
