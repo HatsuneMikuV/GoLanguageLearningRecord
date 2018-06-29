806c56522acf3f4b250583a568bf5ead44f39678
Icarus <anglemiku_v@163.com>
2018-06-29 17:05:31 +0800

    update

--- go语言圣经/word/word.go
+++ go语言圣经/word/word.go
@@ -1,11 +1,28 @@
 // Package word provides utilities for word games.
 package word
 
+import "unicode"
+
 // IsPalindrome reports whether s reads the same forward and backward.
 // (Our first attempt.)
+//Letter case is ignored, as are non-letters.
 func IsPalindrome(s string) bool {
-	for i := range s {
-		if s[i] != s[len(s)-1-i] {
+	//first
+	//for i := range s {
+	//	if s[i] != s[len(s)-1-i] {
+	//		return false
+	//	}
+	//}
+
+	//second
+	var letters []rune
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			letters = append(letters, unicode.ToLower(r))
+		}
+	}
+	for i := range letters {
+		if letters[i] != letters[len(letters)-1-i] {
 			return false
 		}
 	}
